7633f3f70505b700696d21b7e1b34ae4b8296fdd
talip <talipefe35@gmail.com>
2021-11-23 20:42:43 +0300

    23.11.2021

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class _MyAppState extends State<MyApp> {
   var _image;
   List contents=[];
   int no=1;
+   File? firstFile;
 
   Future imageDir() async{
     //Directory tempDir = await getTemporaryDirectory();
@@ -37,20 +38,51 @@ class _MyAppState extends State<MyApp> {
     //Directory tempDir = await getApplicationDocumentsDirectory();
     Directory? tempDir = await getExternalStorageDirectory();
     String tempPath = tempDir!.path;
+    String denemeDirPath='/storage/emulated/0/Android/data/com.example.image_save/files';
+    Directory denemeDir=Directory(denemeDirPath);
+    String denemeFilePath='/storage/emulated/0/Android/data/com.example.image_save/files/denemeFile.jpg';
+    File denemeFile=File(denemeFilePath);
+    //denemeFile.createSync();
+    //print(denemeFile.statSync());
+    print(denemeFile);
     contents=tempDir.listSync();
-    final File firstFile=contents[1];
-    print(firstFile);
+    if(contents.isNotEmpty){
+      firstFile=contents[0];
+      //print('Deneme  $firstFile');
+      //print(firstFile!.lastAccessedSync());
+    }
+    else{
+      print('Resim yok.');
+    }
+    //print(firstFile);
+    //String firstFileName=p.basename(firstFile!.path)
+    //String firstFileName=p.dirname(tempDir.path);
+    //String firstFileName=p.basenameWithoutExtension(tempPath); // files
+    //String firstFileName=p.basename(tempPath);
+    //String firstFileName=p.split(tempPath).last; //files
+    //String firstFileName=p.split(tempPath).first;  //  /
+    //String firstFileName=p.extension(firstFile!.path);  //  .jpg
+    //String firstFileName=p.withoutExtension(firstFile!.path);  //storage/emulated/0/Android/data/com.example.image_save/files/deneme3
+    //String firstFileName=p.basename(denemeDirPath);
+    //print(firstFileName);
+    //firstFile.delete();
+    //final File firstFileCopy=firstFile!.copySync('$tempPath/deneme4.jpg');
     //final File firstFileRename=firstFile.renameSync('$tempPath/deneme1.jpg');
     //final File firstFileRename=firstFile.renameSync('$tempPath/deneme2.jpg');
-    contents.removeAt(2);
+
+    //contents.removeAt(1);
+    /*setState(() {
+      //contents=tempDir.listSync();
+      contents=denemeDir.listSync();
+    });*/
     contents.forEach((element) {
       if(element is File){
         print('$no  $element');
         no++;
       }
     });
-    print(tempPath);
-    print(contents.length);
+    //print(tempPath);
+    //print(contents.length);
   }
 
   Future galery() async{
